e8544ef9ab29f609fef1143b4ce7b68b86357463
Elvirka2k22 <104283848+Elvirka2k22@users.noreply.github.com>
2022-06-28 03:05:01 +0300

    Файлы, списки
    Использование списков для хранения массива данных с произвольным количеством элементов

--- calc/src/file_calc.c
+++ calc/src/file_calc.c
@@ -9,179 +9,269 @@
  */
 
 // Тимербаева Эльвира Ильсур
+
 #include <stdio.h>
 #include <stdlib.h>
-char regul;
-char inputFileName[259], outputFileName[259];
-char h;
+
+// основная структура данных
+typedef struct calcEl
+{
+	char rezhim;
+	char c;
+	double result;
+	double *x,*y;
+	double *resultOperation;
+	int size;
+	struct calcEl *next;
+} calcEl;
+
+// Структура для хранения текущему и главного значения
+typedef struct listCalcEl
+{
+	calcEl *headValue;
+	calcEl *currentValue;
+} listCalcEl;
+
+// Стартовая функция инициализации значений
+void initListCalc (listCalcEl *val)
+{
+	val->headValue = NULL;
+	val->currentValue = NULL;
+}
+
+void pushCalc (listCalcEl *val, calcEl *dataCalc)
+{
+	calcEl* tempInfo = malloc (sizeof(calcEl));
+	calcEl *last = val->headValue;
+	tempInfo->x = dataCalc->x;
+	tempInfo->y = dataCalc->y;
+	tempInfo->c = dataCalc->c;
+	tempInfo->rezhim = dataCalc->rezhim;
+	tempInfo->size = dataCalc->size;
+	tempInfo->resultOperation = dataCalc->resultOperation;
+	tempInfo->next = NULL;
+	tempInfo->result = dataCalc->result;
+	
+	if (val->headValue == NULL)
+	{
+		val->headValue = tempInfo;
+		return;
+	}
+	
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+	last->next = tempInfo;
+	return;
+}
+
+
+
+calcEl* nextCalcValue (listCalcEl *l)
+{
+	l->currentValue = l->currentValue->next;
+	return l->currentValue;
+}
+
+void deleteCalc (listCalcEl *l)
+{
+	calcEl *temp;
+	if (&l->headValue == NULL) return;
+	temp = l->headValue;
+	l->headValue = l->headValue->next;
+	free (temp);
+}
+
 FILE *fileInput,*fileOut;
 int main(int argc, char *argv[])
 {
 	setvbuf(stdout, NULL, _IONBF, 0);
 	setvbuf(stderr, NULL, _IONBF, 0);
+	
+	char inputFileName[259], outputFileName[259];
+	char h; double d, resultValueCalc;
+	double *result;
+
+	calcEl *var;
+	listCalcEl lCalc1,lCalc2;
+	
 	do
 	{
+		// Инициализуем 
+		initListCalc (&lCalc1);
+		initListCalc (&lCalc2);
 		puts ("Имя входного файла");
 		scanf ("%s", inputFileName);
 
 		puts ("Имя выходного файла");
 		scanf ("%s", outputFileName);
-        // Открываем файл на чтение и собираем данные
+
 		fileInput = fopen (inputFileName, "r");
-		if ((fileOut=fopen(outputFileName, "a"))==NULL)
-			{
-                // В случае отсутствия файла для записи - создаем, открыв на запись
-				fileOut=fopen(outputFileName, "w");
+		var = malloc (sizeof (calcEl));
+		while (feof(fileInput) == 0) {
+			fscanf (fileInput, " %c %c", &var->c, &var->rezhim);
+			switch (var->rezhim) { // Режим работы (векторный, стандартный)
+				case 'V':
+					fscanf (fileInput, "%i", &var->size); // Вытаскиваем длину вектора
+					// Выделили
+					var->x = malloc (var->size*sizeof(double));
+					var->y = malloc (var->size*sizeof(double));
+					// Считали информацию по векторам
+					for (int i=0; i < var->size; i++) 
+						fscanf (fileInput, "%lf", &var->x[i]);
+					for (int i=0; i < var->size; i++) 
+						fscanf (fileInput, "%lf", &var->y[i]);
+					break;
+				case 'N':
+					var->size = 1;
+					if (var->c == '!')
+					{
+						var->x = malloc (sizeof (double));
+						fscanf (fileInput, "%lf", &var->x[0]);
+						var->y = NULL;
+					} else {
+						var->x = malloc (sizeof(double));
+						var->y = malloc (sizeof(double));
+						fscanf (fileInput, "%lf", &var->x[0]);
+						fscanf (fileInput, "%lf", &var->y[0]);
+					}
+					break;
 			}
-		while (feof(fileInput) == 0)
-		{
-            // Идем через весь входящий файл
-			double a, b, d; // задаем переменные типа double
-			char c; // задаем переменную типа char
-			char rezhime;
-            // Определяем режим работы калькулятора (векторный, обычный)
-			fscanf (fileInput, " %c %c", &c, &rezhime);
-			switch (rezhime)
-			{
-                case 'V':
-                {
-                    int lVector; // длина вектора
-                    fscanf (fileInput, "%i", &lVector); //считываем длину вектора
-                    switch (c){
-                        case '+':
-                        {
-                            double *f,*g;
-                            f = malloc(lVector*sizeof(double)); // память для хранения вектора
-                            g = malloc(lVector*sizeof(double));
-                            fprintf (fileOut, "(");
-                            for (int i = 0; i < lVector; i++) //считываем вектор
-                            {
-                                fscanf (fileInput, "%lf", &f[i]);
-                                fprintf (fileOut, "%lf ", f[i]);
-                            }
-                            fprintf (fileOut, ") + (");
-                            for (int i = 0; i<lVector; i++)
-                            {
-                                fscanf (fileInput, "%lf", &g[i]);
-                                fprintf (fileOut, "%lf ", g[i]);
-                            }
-                            fprintf (fileOut, ") = (");
-                            for (int i = 0; i<lVector; i++)
-                            {
-                                fprintf (fileOut, "%lf ", f[i]+g[i]);
-                            }
-                            fprintf (fileOut, ")\n");
-                            free (f);
-                            free (g);
-                            break;
-                        }
-                        case '-':
-                        {
-                            double *f,*g;
-                            f=malloc(lVector*sizeof(double));
-                            g=malloc(lVector*sizeof(double));
-                            fprintf (fileOut, "(");
-                            for (int i = 0; i<lVector; i++) //считываем вектор
-                            {
-                                fscanf (fileInput, "%lf", &f[i]);
-                                fprintf (fileOut, "%lf ", f[i]);
-                            }
-                            fprintf (fileOut, ") - (");
-                            for (int i = 0; i<lVector; i++)
-                            {
-                                fscanf (fileInput, "%lf", &g[i]);
-                                fprintf (fileOut, "%lf ", g[i]);
-                            }
-                            fprintf (fileOut, ") = (");
-                            for (int i = 0; i<lVector; i++)
-                            {
-                                fprintf (fileOut, "%lf ", f[i]-g[i]);
-                            }
-                            fprintf (fileOut, ")\n");
-                            free (f);
-                            free (g);
-                            break;
-                        }
-                        case '*':
-                        {
-                            double *f,*g;
-                            f=malloc(lVector*sizeof(double));
-                            g=malloc(lVector*sizeof(double));
-                            fprintf (fileOut, "(");
-                            for (int i=0; i<lVector; i++) //считываем вектор
-                            {
-                                fscanf (fileInput, "%lf", &f[i]);
-                                fprintf (fileOut, "%lf ", f[i]);
-                            }
-                            fprintf (fileOut, ") * (");
-                            for (int i=0; i<lVector; i++)
-                            {
-                                fscanf (fileInput, "%lf", &g[i]);
-                                fprintf (fileOut, "%lf ", g[i]);
-                            }
-                            fprintf (fileOut, ") = ");
-                            double resultVec; //задаем переменную для хранения результата
-                            for (int i=0; i<lVector; i++)
-                            {
-                                resultVec += f[i] * g[i];
-                            }
-                            fprintf (fileOut, "%lf\n", resultVec);
-                            resultVec = 0;
-                            free (f);
-                            free (g);
-                            break;
-                        }
-                    }
-                    break;
-                }
-                case 'N':
-                {
-                    switch (c) //блок функций
-                    {
-                        case '+': // Если ищем сумму
-                            fscanf (fileInput, "%lg %lg", &a, &b);
-                            fprintf (fileOut, "%lg + %lg = %lg\n", a,b,a+b);
-                            break;
-                        case '-': // Если ищем разность
-                            fscanf (fileInput, "%lg %lg", &a, &b);
-                            fprintf (fileOut, "%lg - %lg = %lg\n", a,b,a-b);
-                            break;
-                        case '*': // Если умножаем
-                            fscanf (fileInput, "%lg %lg", &a, &b);
-                            fprintf (fileOut, "%lg * %lg = %lg\n", a,b,a*b);
-                            break;
-                        case '/': // Делим
-                            fscanf (fileInput, "%lg %lg", &a, &b);
-                            fprintf (fileOut, "%lg / %lg = %lg\n", a,b,a/b);
-                            break;
-                        case '^': // Степень
-                            fscanf (fileInput, "%lg %lg", &a, &b);
-                            d = a; // временное хранение
-                            for (int i = 1; i < b; i++)
-                            {
-                                d = d * a;
-                            }
-                            fprintf (fileOut, "%lg ^ %lg = %lg\n", a, b, d);
-                            break;
-                        case '!': // блок факториала
-                            d = 1; // приравниваем, чтобы посчитать факториал
-                            fscanf (fileInput, "%lg", &a);
-                            if (a >= 0) // так как факториал 0=1, сделаем такой цикл
-                            {
-                                for (int i = 0; i<a; i++)
-                                {
-                                    d = d * (i + 1);
-                                }
-                                fprintf (fileOut, "%lg! = %lg\n", a,d);
-                            }
-                            break;
-                    }
-                    break;
-			    }
-			}
+			pushCalc (&lCalc1, var);
 		}
 		fclose (fileInput);
+		lCalc1.currentValue = lCalc1.headValue;
+		while (lCalc1.currentValue != NULL) {
+			var->rezhim = lCalc1.currentValue->rezhim;
+			
+			var->x = lCalc1.currentValue->x;
+			var->y = lCalc1.currentValue->y;
+			var->c = lCalc1.currentValue->c;
+			var->result = lCalc1.currentValue->result;
+			var->resultOperation = lCalc1.currentValue->resultOperation;
+			var->size = lCalc1.currentValue->size;
+			switch (var->rezhim) {
+				case 'V':
+					switch (var->c) {
+						case '+':
+							result = malloc (var->size*sizeof(double));
+							for (int i=0; i<var->size; i++) 
+								result[i] = var->x[i]+var->y[i];
+							var->resultOperation = result;
+							break;
+						case '-':
+							for (int i=0; i<var->size; i++) 
+								result[i] = var->x[i]-var->y[i];
+							var->resultOperation = result;
+							break;
+						case '*':
+							for (int i=0; i<var->size; i++) {
+								resultValueCalc += var->x[i] * var->y[i];
+							}
+							var->result = resultValueCalc;
+							break;
+					}
+					break;
+				case 'N':
+					switch (var->c) {
+						case '+': // блок суммы
+							var->result = var->x[0] + var->y[0];
+							break;
+						case '-': // блок разности
+							var->result = var->x[0] - var->y[0];
+							break;
+						case '*': // блок умножения
+							var->result = var->x[0] * var->y[0];
+							break;
+						case '/': // блок деления
+							var->result = var->x[0] / var->y[0];
+							break;
+						case '^': // блок возведения в степень
+							d=var->x[0]; // приравниваем, чтобы посчитать степень
+							for (int i=1; i < var->y[0]; i++)
+							{
+								d = d * var->x[0];
+							}
+							var->result = d;
+							break;
+						case '!': // блок факториала
+							d=1; // приравниваем, чтобы посчитать факториал
+							if (var->x[0] >= 0) // так как факториал 0=1, сделаем такой цикл
+							{
+								for (int i=0; i < var->x[0]; i++)
+								{
+									d = d * (i+1);
+								}
+							}
+							var->result = d;
+							break;
+					}
+					break;
+			}
+			pushCalc (&lCalc2, var);
+			nextCalcValue (&lCalc1);
+		}
+		if ((fileOut = fopen(outputFileName, "a")) == NULL) {
+			fileOut = fopen(outputFileName, "w");
+		}
+		free (var);
+
+		lCalc1.currentValue = lCalc1.headValue;
+		lCalc2.currentValue = lCalc2.headValue;
+		
+		while (lCalc2.currentValue != NULL) {
+			switch (lCalc2.currentValue->rezhim) {
+				case 'V':
+					fprintf (fileOut, "( ");
+					for (int i = 0; i < lCalc2.currentValue->size; i++) {
+						if (i == lCalc2.currentValue->size-1) {
+							fprintf (fileOut, "%lf", lCalc2.currentValue->x[i]);
+						}
+						else 
+							fprintf (fileOut, "%lf ", lCalc2.currentValue->x[i]);
+					}
+					fprintf (fileOut, " ) %c ( ", lCalc2.currentValue->c);
+					for (int i = 0; i < lCalc2.currentValue->size; i++) {
+						if (i == lCalc2.currentValue->size-1) {
+							fprintf (fileOut, "%lf", lCalc2.currentValue->y[i]);
+						}
+						else 
+							fprintf (fileOut, "%lf ", lCalc2.currentValue->y[i]);
+					}
+					if (lCalc2.currentValue->c == '+' || lCalc2.currentValue->c == '-') {
+						fprintf (fileOut, " ) = ( ");
+						for (int i=0; i < lCalc2.currentValue->size; i++) {
+							if (i == lCalc2.currentValue->size-1) {
+								fprintf (fileOut, "%lf", lCalc2.currentValue->resultOperation[i]);
+							}
+							else 
+								fprintf (fileOut, "%lf ", lCalc2.currentValue->resultOperation[i]);
+						}
+						fprintf (fileOut, " )\n");
+					}
+					else 
+						fprintf (fileOut, " ) = %lf\n", lCalc2.currentValue->result);
+					break;
+				case 'N':
+					if (lCalc2.currentValue->c == '!') {
+						fprintf (fileOut, "%lf! = %lf\n", *lCalc2.currentValue->x, lCalc2.currentValue->result);
+					}
+					else 
+						fprintf (fileOut, "%lf %c %lf = %lf\n", *lCalc2.currentValue->x, lCalc2.currentValue->c, *lCalc2.currentValue->y, lCalc2.currentValue->result);
+					break;
+				}
+			nextCalcValue (&lCalc2);
+		}
 		fclose (fileOut);
+		while (lCalc1.headValue != NULL) {
+			deleteCalc (&lCalc1);
+		}
+
+		while (lCalc2.headValue != NULL) {
+			deleteCalc (&lCalc2);
+		}
+
 		puts ("\nДля продолжения введите y, n для выхода");
 		scanf (" %c", &h);
 	}
